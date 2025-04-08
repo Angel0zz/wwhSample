689a650d0a94d611346eddf2fd1cbcb18f23bcda
Angel0zz <regidor.angelo.r@gmail.com>
2025-04-08 12:26:18 +0800

    home  update revision

--- script/OnScrollAnimtion.js
+++ script/OnScrollAnimtion.js
@@ -13,22 +13,3 @@ var currentScrollPos = window.pageYOffset;
   prevScrollpos = currentScrollPos;
 }
 
-
-
-const listItems = document.querySelectorAll('.nav-link');
-const fromContainer = document.querySelector('.main-bg');
-
-// Adding hover effect to each li
-listItems.forEach(item => {
-  item.addEventListener('mouseenter', function() {
-      const bgImage = item.getAttribute('data-bg');
-      fromContainer.style.backgroundImage = `
-        radial-gradient(circle, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.1) 20%),
-        url('/img-assets/${bgImage}')
-      `;
-  });
-  item.addEventListener('mouseleave', function() {
-      // Reset to default image when mouse leaves the list item
-      fromContainer.style.backgroundcolor = 'radial-gradient(circle, rgba(0, 0, 0, 1) 100%, rgba(0, 0, 0, 1) 100%),url("/img-assets/279951970.jpg")';
-  });
-});
